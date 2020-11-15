a086ddd061369012142337accc2a18241236e35e
Semih Özköroğlu <semihozkoroglu@gmail.com>
2020-11-15 23:05:29 +0300

    Bismillahirrahmanirrahim

---
+++ README.md
@@ -0,0 +1,3 @@
+# rbs-android
+
+[![](https://jitpack.io/v/rettersoft/rbs-android.svg)](https://jitpack.io/#rettersoft/rbs-android)
---
+++ rbs/src/main/java/com/rettermobile/rbs/RBSService.kt
@@ -0,0 +1,10 @@
+package com.rettermobile.rbs
+
+/**
+ * Created by semihozkoroglu on 15.11.2020.
+ */
+class RBSService constructor(val projectId: String) {
+
+    fun authenticate() {
+    }
+}
