6d66258dba84c3ed2c68c1b493c36723fc70d0c7
Semih Özköroğlu <semih.ozkoroglu@softtechventures.com>
2022-02-24 14:07:15 +0300

    :bug: create new document in registration

--- rio/src/main/java/com/rettermobile/rio/cloud/RioCloudObjectState.kt
+++ rio/src/main/java/com/rettermobile/rio/cloud/RioCloudObjectState.kt
@@ -1,5 +1,6 @@
 package com.rettermobile.rio.cloud
 
+import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.ListenerRegistration
 import com.rettermobile.rio.RioConfig
 import com.rettermobile.rio.RioFirebaseManager
@@ -14,28 +15,20 @@ sealed class RioCloudObjectState constructor(params: RioCloudObjectParams) {
     val instanceId = params.instanceId
 
     private var listener: ListenerRegistration? = null
+    private var document: DocumentReference? = null
 
     private var successEvent: ((String?) -> Unit)? = null
     private var errorEvent: ((Throwable?) -> Unit)? = null
 
+    private var path = ""
+
     init {
-        var path = "/projects/${RioConfig.projectId}/classes/$classId/instances/$instanceId/"
+        path = "/projects/${RioConfig.projectId}/classes/$classId/instances/$instanceId/"
 
         if (this is RioCloudUserObjectState) {
             path += "userState/${TokenManager.userId}"
         } else if (this is RioCloudRoleObjectState) {
             path += "roleState/${TokenManager.userIdentity}"
-        }
-
-        listener?.remove()
-
-        val document = RioFirebaseManager.getDocument(path)
-        listener = document.addSnapshotListener { value, error ->
-            if (error != null) {
-                errorEvent?.invoke(error)
-            } else {
-                successEvent?.invoke(value?.data?.toString())
-            }
         }
     }
 
@@ -45,6 +38,19 @@ sealed class RioCloudObjectState constructor(params: RioCloudObjectParams) {
     ) {
         successEvent = eventFired
         errorEvent = errorFired
+
+        if (document == null) {
+            document = RioFirebaseManager.getDocument(path)
+        }
+
+        listener?.remove()
+        listener = document?.addSnapshotListener { value, error ->
+            if (error != null) {
+                errorEvent?.invoke(error)
+            } else {
+                successEvent?.invoke(value?.data?.toString())
+            }
+        }
     }
 
     fun removeListener() {
