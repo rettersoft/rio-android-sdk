ee1896c9b1464658a1750bdf68542916b4c04ea4
Semih Özköroğlu <semih.ozkoroglu@softtechventures.com>
2022-09-12 15:41:38 +0300

    - ssl pinnig added for custom domain

--- rio/src/main/java/com/rettermobile/rio/service/RioNetwork.kt
+++ rio/src/main/java/com/rettermobile/rio/service/RioNetwork.kt
@@ -22,7 +22,7 @@ class RioNetwork {
     private var cloudService: RioCloudService? = null
 
     private fun provideCertificate(): CertificatePinner {
-        return CertificatePinner.Builder()
+        val certificate = CertificatePinner.Builder()
             .add("*.rtbs.io", "sha256/++MBgDH5WGvL9Bcn5Be30cRcL0f5O+NyoXuWtQdX1aI=")
             .add("*.rtbs.io", "sha256/f0KW/FtqTjs108NpYj42SrGvOB2PpxIVM8nWxjPqJGE=")
             .add("*.rtbs.io", "sha256/NqvDJlas/GRcYbcWE8S/IceH9cq77kg0jVhZeAPXq8k=")
@@ -41,7 +41,19 @@ class RioNetwork {
             .add("*.retter.io", "sha256/9+ze1cZgR9KO1kZrVDxA4HQ6voHRCSVNz4RdTCx4U8U=")
             .add("*.retter.io", "sha256/KwccWaCgrnaw6tsrrSO61FgLacNgG2MMLq8GE6+oP5I=")
             .add("*.retter.io", "sha256/FfFKxFycfaIz00eRZOgTf+Ne4POK6FgYPwhBDqgqxLQ=")
-            .build()
+
+        if (!RioConfig.config.customDomain.isNullOrEmpty()) {
+            val customDomain = RioConfig.config.customDomain!!.replace("https://", "").replace("http://", "")
+
+            certificate.add(customDomain, "sha256/++MBgDH5WGvL9Bcn5Be30cRcL0f5O+NyoXuWtQdX1aI=")
+            certificate.add(customDomain, "sha256/f0KW/FtqTjs108NpYj42SrGvOB2PpxIVM8nWxjPqJGE=")
+            certificate.add(customDomain, "sha256/NqvDJlas/GRcYbcWE8S/IceH9cq77kg0jVhZeAPXq8k=")
+            certificate.add(customDomain, "sha256/9+ze1cZgR9KO1kZrVDxA4HQ6voHRCSVNz4RdTCx4U8U=")
+            certificate.add(customDomain, "sha256/KwccWaCgrnaw6tsrrSO61FgLacNgG2MMLq8GE6+oP5I=")
+            certificate.add(customDomain, "sha256/FfFKxFycfaIz00eRZOgTf+Ne4POK6FgYPwhBDqgqxLQ=")
+        }
+
+        return certificate.build()
     }
 
     private fun provideOkHttp(): OkHttpClient {
